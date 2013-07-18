138c1e91b41dbc894dcb37cf3e3f01dc158944d8
Tero Kristo <t-kristo@ti.com>
2013-07-18 17:15:51 +0300

    CLK: TI: add omap5 clock init file
    clk-54xx.c now contains the clock init functionality for omap5, including
    DT clock registration and adding of static clkdev entries.
    
    Signed-off-by: Tero Kristo <t-kristo@ti.com>
    Tested-by: Nishanth Menon <nm@ti.com>
    Acked-by: Tony Lindgren <tony@atomide.com>

--- include/linux/clk/ti.h
+++ include/linux/clk/ti.h
@@ -225,6 +225,7 @@ int of_ti_autoidle_setup(struct device_node *node, struct clk_reg_ops *ops);
 int ti_clk_add_component(struct device_node *node, struct clk_hw *hw, int type);
 
 int omap4xxx_dt_clk_init(void);
+int omap5xxx_dt_clk_init(void);
 
 #ifdef CONFIG_OF
 void of_ti_clk_allow_autoidle_all(void);
