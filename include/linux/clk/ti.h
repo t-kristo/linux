93e394e15af636ce9a77821f8d540660603e3dec
Tero Kristo <t-kristo@ti.com>
2013-08-02 14:04:19 +0300

    CLK: TI: add omap3 clock init file
    clk-3xxx.c now contains the clock init functionality for omap3, including
    DT clock registration and adding of static clkdev entries.
    
    Signed-off-by: Tero Kristo <t-kristo@ti.com>
    Tested-by: Nishanth Menon <nm@ti.com>
    Acked-by: Tony Lindgren <tony@atomide.com>

--- include/linux/clk/ti.h
+++ include/linux/clk/ti.h
@@ -218,6 +218,7 @@ int omap3_dpll4_set_rate(struct clk_hw *clk, unsigned long rate,
 int omap2_dflt_clk_enable(struct clk_hw *hw);
 void omap2_dflt_clk_disable(struct clk_hw *hw);
 int omap2_dflt_clk_is_enabled(struct clk_hw *hw);
+void omap3_clk_lock_dpll5(void);
 
 void ti_dt_clocks_register(struct ti_dt_clk *oclks);
 void ti_dt_clk_init_provider(struct device_node *np, struct regmap *regmap);
@@ -225,6 +226,10 @@ void ti_dt_clockdomains_setup(void);
 int of_ti_autoidle_setup(struct device_node *node, struct regmap *regmap);
 int ti_clk_add_component(struct device_node *node, struct clk_hw *hw, int type);
 
+int omap3430_dt_clk_init(void);
+int omap3630_dt_clk_init(void);
+int am35xx_dt_clk_init(void);
+int ti81xx_dt_clk_init(void);
 int omap4xxx_dt_clk_init(void);
 int omap5xxx_dt_clk_init(void);
 int dra7xx_dt_clk_init(void);
