19dd869ec434d710e994721b4aa88a93bda71c18
Tero Kristo <t-kristo@ti.com>
2013-09-06 10:06:52 +0300

    hack: change gptimer mapping in board generic for beagle
    Signed-off-by: Tero Kristo <t-kristo@ti.com>

--- arch/arm/mach-omap2/board-generic.c
+++ arch/arm/mach-omap2/board-generic.c
@@ -143,7 +143,7 @@ DT_MACHINE_START(OMAP3_GP_DT, "Generic OMAP3-GP (Flattened Device Tree)")
 	.handle_irq	= omap3_intc_handle_irq,
 	.init_machine	= omap_generic_init,
 	.init_late	= omap3_init_late,
-	.init_time	= omap3_secure_sync32k_timer_init,
+	.init_time	= omap3_sync32k_timer_init,
 	.dt_compat	= omap3_gp_boards_compat,
 	.restart	= omap3xxx_restart,
 MACHINE_END
