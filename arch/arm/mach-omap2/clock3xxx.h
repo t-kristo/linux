7f4cb95a1f3742207585436efd3680d1cf1ec937
Tero Kristo <t-kristo@ti.com>
2014-02-25 11:42:49 +0200

    ARM: OMAP3: clock: remove legacy clock data
    This is no longer needed as the clock data is provided through DT.
    
    Signed-off-by: Tero Kristo <t-kristo@ti.com>

--- arch/arm/mach-omap2/clock3xxx.h
+++ arch/arm/mach-omap2/clock3xxx.h
@@ -8,13 +8,10 @@
 #ifndef __ARCH_ARM_MACH_OMAP2_CLOCK3XXX_H
 #define __ARCH_ARM_MACH_OMAP2_CLOCK3XXX_H
 
-int omap3xxx_clk_init(void);
 int omap3_core_dpll_m2_set_rate(struct clk_hw *clk, unsigned long rate,
 					unsigned long parent_rate);
 
 extern struct clk *sdrc_ick_p;
 extern struct clk *arm_fck_p;
-
-extern const struct clkops clkops_noncore_dpll_ops;
 
 #endif
