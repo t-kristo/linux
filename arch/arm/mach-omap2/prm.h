1ded907a3838c9479aaf102bbe9cab1cb289b83a
Tero Kristo <t-kristo@ti.com>
2014-03-28 16:19:50 +0200

    ARM: OMAP2+: PRCM: add prcm_early init call for DT boot
    This parses the prm, cm and scrm base addresses from DT if available,
    avoiding the need for various omap2_set_globals_* calls.
    
    Signed-off-by: Tero Kristo <t-kristo@ti.com>

--- arch/arm/mach-omap2/prm.h
+++ arch/arm/mach-omap2/prm.h
@@ -20,7 +20,7 @@ extern void __iomem *prm_base;
 extern u16 prm_dev_inst;
 extern u16 prm_features;
 extern void omap2_set_globals_prm(void __iomem *prm);
-int of_prcm_init(void);
+int of_prcm_late_init(void);
 # endif
 
 /*
