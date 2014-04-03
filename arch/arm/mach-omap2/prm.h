eb930f3c04f653e6c717a9c130dd725f31a060d2
Tero Kristo <t-kristo@ti.com>
2014-04-03 14:48:05 +0300

    ARM: OMAP2+: PRM: add generic API for checking hardreset status
    PRM driver now has a generic API for checking hardreset status. SoC
    specific support functions are registered through the prm_ll_data.
    
    Signed-off-by: Tero Kristo <t-kristo@ti.com>

--- arch/arm/mach-omap2/prm.h
+++ arch/arm/mach-omap2/prm.h
@@ -155,7 +155,8 @@ struct prm_ll_data {
 	int (*assert_hardreset)(u8 shift, u8 part, s16 prm_mod, u16 offset);
 	int (*deassert_hardreset)(u8 shift, u8 st_shift, u8 part, s16 prm_mod,
 				  u16 offset, u16 st_offset);
-				  
+	int (*is_hardreset_asserted)(u8 shift, u8 part, s16 prm_mod,
+				     u16 offset);			  
 };
 
 extern int prm_register(struct prm_ll_data *pld);
@@ -164,6 +165,7 @@ extern int prm_unregister(struct prm_ll_data *pld);
 int prm_assert_hardreset(u8 shift, u8 part, s16 prm_mod, u16 offset);
 int prm_deassert_hardreset(u8 shift, u8 st_shift, u8 part, s16 prm_mod,
 			   u16 offset, u16 st_offset);
+int prm_is_hardreset_asserted(u8 shift, u8 part, s16 prm_mod, u16 offset);
 extern u32 prm_read_reset_sources(void);
 extern bool prm_was_any_context_lost_old(u8 part, s16 inst, u16 idx);
 extern void prm_clear_context_loss_flags_old(u8 part, s16 inst, u16 idx);
--- arch/arm/mach-omap2/prm2xxx_3xxx.h
+++ arch/arm/mach-omap2/prm2xxx_3xxx.h
@@ -53,7 +53,8 @@
 #include "powerdomain.h"
 
 /* These omap2_ PRM functions apply to both OMAP2 and 3 */
-extern int omap2_prm_is_hardreset_asserted(s16 prm_mod, u8 shift);
+extern int omap2_prm_is_hardreset_asserted(u8 shift, u8 part, s16 prm_mod,
+					   u16 offset);
 extern int omap2_prm_assert_hardreset(u8 shift, u8 part, s16 prm_mod,
 				      u16 offset);
 extern int omap2_prm_deassert_hardreset(u8 rst_shift, u8 st_shift, u8 part,
