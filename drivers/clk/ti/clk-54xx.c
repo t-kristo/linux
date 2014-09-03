fa52a492b8a1376c4de907a9c735b5bb360ef93c
Tero Kristo <t-kristo@ti.com>
2014-09-03 15:16:45 +0300

    clk: ti: omap5: use assigned-clocks for boot clock setup
    Use assigned-clocks feature from the device tree instead of hardcoding
    the default clock rates / parents in the driver.
    
    Signed-off-by: Tero Kristo <t-kristo@ti.com>

--- drivers/clk/ti/clk-54xx.c
+++ drivers/clk/ti/clk-54xx.c
@@ -17,14 +17,6 @@
 #include <linux/io.h>
 #include <linux/clk/ti.h>
 
-#define OMAP5_DPLL_ABE_DEFFREQ				98304000
-
-/*
- * OMAP543x TRM, section "3.6.3.9.5 DPLL_USB Preferred Settings"
- * states it must be at 960MHz
- */
-#define OMAP5_DPLL_USB_DEFFREQ				960000000
-
 static struct ti_dt_clk omap54xx_clks[] = {
 	DT_CLK(NULL, "pad_clks_src_ck", "pad_clks_src_ck"),
 	DT_CLK(NULL, "pad_clks_ck", "pad_clks_ck"),
@@ -224,37 +216,9 @@ static struct ti_dt_clk omap54xx_clks[] = {
 
 int __init omap5xxx_dt_clk_init(void)
 {
-	int rc;
-	struct clk *abe_dpll_ref, *abe_dpll, *sys_32k_ck, *usb_dpll;
-
 	ti_dt_clocks_register(omap54xx_clks);
 
 	omap2_clk_disable_autoidle_all();
-
-	abe_dpll_ref = clk_get_sys(NULL, "abe_dpll_clk_mux");
-	sys_32k_ck = clk_get_sys(NULL, "sys_32k_ck");
-	rc = clk_set_parent(abe_dpll_ref, sys_32k_ck);
-	abe_dpll = clk_get_sys(NULL, "dpll_abe_ck");
-	if (!rc)
-		rc = clk_set_rate(abe_dpll, OMAP5_DPLL_ABE_DEFFREQ);
-	if (rc)
-		pr_err("%s: failed to configure ABE DPLL!\n", __func__);
-
-	abe_dpll = clk_get_sys(NULL, "dpll_abe_m2x2_ck");
-	if (!rc)
-		rc = clk_set_rate(abe_dpll, OMAP5_DPLL_ABE_DEFFREQ * 2);
-	if (rc)
-		pr_err("%s: failed to configure ABE m2x2 DPLL!\n", __func__);
-
-	usb_dpll = clk_get_sys(NULL, "dpll_usb_ck");
-	rc = clk_set_rate(usb_dpll, OMAP5_DPLL_USB_DEFFREQ);
-	if (rc)
-		pr_err("%s: failed to configure USB DPLL!\n", __func__);
-
-	usb_dpll = clk_get_sys(NULL, "dpll_usb_m2_ck");
-	rc = clk_set_rate(usb_dpll, OMAP5_DPLL_USB_DEFFREQ/2);
-	if (rc)
-		pr_err("%s: failed to set USB_DPLL M2 OUT\n", __func__);
 
 	return 0;
 }
