91b7dc7ec2f67437c8a6b0e274d2d56c1d0d8687
Tero Kristo <t-kristo@ti.com>
2015-02-24 16:22:45 +0200

    ARM: dts: omap3: merge control module features under scrm node
    This patch moves all the control module related features under scrm node.
    scm_conf area is changed to use syscon, and also the clocks are moved
    under scm_conf area.
    
    Signed-off-by: Tero Kristo <t-kristo@ti.com>

--- arch/arm/mach-omap2/control.c
+++ arch/arm/mach-omap2/control.c
@@ -670,7 +670,7 @@ static const struct of_device_id omap_scrm_dt_match_table[] = {
 	{ .compatible = "ti,am3-scrm", .data = &ctrl_data },
 	{ .compatible = "ti,am4-scrm", .data = &ctrl_data },
 	{ .compatible = "ti,omap2-scrm", .data = &omap2_ctrl_data },
-	{ .compatible = "ti,omap3-scrm", .data = &ctrl_data },
+	{ .compatible = "ti,omap3-scrm", .data = &omap2_ctrl_data },
 	{ }
 };
 
