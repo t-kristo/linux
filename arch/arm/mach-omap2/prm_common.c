73aab6f74f320d4b0f195cb02990da18d948c2a0
Tero Kristo <t-kristo@ti.com>
2015-03-03 13:47:08 +0200

    ARM: OMAP2+: clock: add support for specific CM ops to ti_clk_ll_ops
    Clock driver requires access to some CM API functions once the code
    is being moved under the clock driver from the platform directory.
    Gate type clock requires access to cm_wait_module_ready and
    cm_split_idlest_reg functions, which are both used for waiting until
    the module being clocked has been successfully activated. These CM
    APIs are now exported through the ti_clk_ll_ops struct.
    
    Signed-off-by: Tero Kristo <t-kristo@ti.com>

--- arch/arm/mach-omap2/prm_common.c
+++ arch/arm/mach-omap2/prm_common.c
@@ -626,6 +626,8 @@ static struct ti_clk_ll_ops omap_clk_ll_ops = {
 	.clk_writel = prm_clk_writel,
 	.clkdm_clk_enable = clkdm_clk_enable,
 	.clkdm_clk_disable = clkdm_clk_disable,
+	.cm_wait_module_ready = omap_cm_wait_module_ready,
+	.cm_split_idlest_reg = cm_split_idlest_reg,
 };
 
 int __init of_prcm_init(void)
