7b8cd3a4f6d92a5c317120f0cc7105e163b983e9
Tero Kristo <t-kristo@ti.com>
2015-03-30 14:06:53 +0300

    ARM: OMAP2+: omap_device: remove omap_device_build from __init section
    Some devices are built late in boot with the introduction of hwmod
    data modules, and the omap_device_build() function is required later
    in boot also. Thus, remove it from the __init section.
    
    Signed-off-by: Tero Kristo <t-kristo@ti.com>

--- arch/arm/mach-omap2/omap_device.c
+++ arch/arm/mach-omap2/omap_device.c
@@ -507,10 +507,10 @@ void omap_device_delete(struct omap_device *od)
  * information.  Returns ERR_PTR(-EINVAL) if @oh is NULL; otherwise,
  * passes along the return value of omap_device_build_ss().
  */
-struct platform_device __init *omap_device_build(const char *pdev_name,
-						 int pdev_id,
-						 struct omap_hwmod *oh,
-						 void *pdata, int pdata_len)
+struct platform_device *omap_device_build(const char *pdev_name,
+					  int pdev_id,
+					  struct omap_hwmod *oh,
+					  void *pdata, int pdata_len)
 {
 	struct omap_hwmod *ohs[] = { oh };
 
@@ -535,11 +535,11 @@ struct platform_device __init *omap_device_build(const char *pdev_name,
  * platform_device record.  Returns an ERR_PTR() on error, or passes
  * along the return value of omap_device_register().
  */
-struct platform_device __init *omap_device_build_ss(const char *pdev_name,
-						    int pdev_id,
-						    struct omap_hwmod **ohs,
-						    int oh_cnt, void *pdata,
-						    int pdata_len)
+struct platform_device *omap_device_build_ss(const char *pdev_name,
+					     int pdev_id,
+					     struct omap_hwmod **ohs,
+					     int oh_cnt, void *pdata,
+					     int pdata_len)
 {
 	int ret = -ENOMEM;
 	struct platform_device *pdev;
