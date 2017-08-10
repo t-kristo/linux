0757187a8885d25d2dd95f81684bfa6840073950
Tero Kristo <t-kristo@ti.com>
2017-08-10 16:11:06 +0300

    clk: ti: am43xx: add clkctrl clock data
    Add data for am43xx clkctrl clocks, and register it within the clkctrl
    driver.
    
    Signed-off-by: Tero Kristo <t-kristo@ti.com>

--- drivers/clk/ti/clkctrl.c
+++ drivers/clk/ti/clkctrl.c
@@ -432,6 +432,10 @@ static void __init _ti_omap4_clkctrl_setup(struct device_node *node)
 	if (of_machine_is_compatible("ti,am33xx"))
 		data = am3_clkctrl_data;
 #endif
+#ifdef CONFIG_SOC_AM43XX
+	if (of_machine_is_compatible("ti,am4372"))
+		data = am4_clkctrl_data;
+#endif
 
 	while (data->addr) {
 		if (addr == data->addr)
