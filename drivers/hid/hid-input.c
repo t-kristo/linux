380e832cfe56f7905dfc0798bb69be4e64307077
Mika Westerberg <mika.westerberg@linux.intel.com>
2021-05-10 14:00:08 +0300

    HID: hid-input: Add suffix also for HID_DG_PEN
    This and HID_DG_STYLUS are pretty much the same thing so add suffix for
    HID_DG_PEN too. This makes the input device name look better.
    
    Signed-off-by: Mika Westerberg <mika.westerberg@linux.intel.com>

--- drivers/hid/hid-input.c
+++ drivers/hid/hid-input.c
@@ -1737,6 +1737,7 @@ static struct hid_input *hidinput_allocate(struct hid_device *hid,
 		case HID_GD_MOUSE:
 			suffix = "Mouse";
 			break;
+		case HID_DG_PEN:
 		case HID_DG_STYLUS:
 			suffix = "Pen";
 			break;
